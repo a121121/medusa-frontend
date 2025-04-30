f6fead7f4b62f8495f3d135f851a3a3d9ecdbec2
Abdullah Younas <younas.121.121@gmail.com>
2025-04-30 23:18:15 +0500

    nothing special

--- src/app/[countryCode]/(main)/page.tsx
+++ src/app/[countryCode]/(main)/page.tsx
@@ -3,12 +3,13 @@ import { Metadata } from "next"
 import FeaturedProducts from "@modules/home/components/featured-products"
 import Hero from "@modules/home/components/hero"
 import { listCollections } from "@lib/data/collections"
+import { listProducts } from "@lib/data/products"
 import { getRegion } from "@lib/data/regions"
 
 export const metadata: Metadata = {
-  title: "Medusa Next.js Starter Template",
+  title: "Snuglee Baby",
   description:
-    "A performant frontend ecommerce starter template with Next.js 15 and Medusa.",
+    "An amazing baby care product store with Medusa and Next.js",
 }
 
 export default async function Home(props: {
@@ -23,7 +24,10 @@ export default async function Home(props: {
   const { collections } = await listCollections({
     fields: "id, handle, title",
   })
+
+  const products = await listProducts({ regionId: region?.id })
   // console.log(collections)
+
 
   if (!collections || !region) {
     return null
@@ -35,6 +39,7 @@ export default async function Home(props: {
       <div className="py-12">
         <ul className="flex flex-col gap-x-6">
           <FeaturedProducts collections={collections} region={region} />
+          <div>{JSON.stringify(products)}</div>
         </ul>
       </div>
     </>
